760d0a03f293fe2d56e46c52935f54c82c2cb0af
Henrique Bissoli Malaman Alonso <contato.henrique.bissoli@gmail.com>
2024-09-12 15:11:28 -0300

    Set up logon page

---
+++ lib/views/logon.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Logon extends StatefulWidget {
+  const Logon({super.key});
+
+  @override
+  State<Logon> createState() => _LogonState();
+}
+
+class _LogonState extends State<Logon> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
